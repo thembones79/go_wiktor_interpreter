f42f0911b7997f13334d8433e18557fc6013dae6
thembones79 <michal.szumnarski@gmail.con>
2023-11-11 11:28:06 +0100

    added some TokenTypes

--- token/token.go
+++ token/token.go
@@ -6,3 +6,25 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    // Identifiers + literals
+    IDENT = "IDENT" // add, foobar, x, y, ...
+    INT = "INT" // 123456
+
+    // Operators
+    ASSIGN = "="
+    PLUS = "+"
+
+    // Delimiters
+
+
+
+
+
+
+
+)
